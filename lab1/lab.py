bc5abcf35e77b01900055036fd2a07ada75d6c77
SammySatoro <ivzhenko2014@gmail.com>
2023-10-27 01:26:28 +0300

    task6

--- lab1/lab.py
+++ lab1/lab.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 def task2():
     img1 = cv2.imread("images/chebupizza.jpg", cv2.IMREAD_UNCHANGED)
@@ -11,6 +13,7 @@ def task2():
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def task3():
     vid_cap = cv2.VideoCapture("video/polish-cow.mp4", cv2.CAP_ANY)
@@ -26,6 +29,7 @@ def task3():
         cv2.imshow("polish cow", frame2)
         if cv2.waitKey(1) & 0xFF == 27:
             exit()
+
 
 def task4():
     vid_cap = cv2.VideoCapture("video/polish-cow.mp4", cv2.CAP_ANY)
@@ -43,9 +47,10 @@ def task4():
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-    vid_cap.release()       # is optional here
+    vid_cap.release()  # is optional here
     video_writer.release()  # is optional here
     cv2.destroyAllWindows()
+
 
 def task5():
     img1 = cv2.imread("images/chebupizza.jpg")
@@ -61,4 +66,46 @@ def task5():
     cv2.destroyAllWindows()
 
 
-task5()
+def task6():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        height, width, _ = frame.shape
+        image = np.ones((height, width, 3), dtype=np.uint8) * 255
+        center_x, center_y = width // 2, height // 2
+        vertical_left_top = [(center_x - 15, center_y - 100), (center_x - 15, center_y - 15)]
+        vertical_right_top = [(center_x + 15, center_y - 100), (center_x + 15, center_y - 15)]
+        vertical_top = [(center_x - 15, center_y - 100), (center_x + 15, center_y - 100)]
+        vertical_left_bot = [(center_x - 15, center_y + 15), (center_x - 15, center_y + 100)]
+        vertical_right_bot = [(center_x + 15, center_y + 15), (center_x + 15, center_y + 100)]
+        vertical_bottom = [(center_x - 15, center_y + 100), (center_x + 15, center_y + 100)]
+        horizontal_left = [(center_x - 80, center_y - 15), (center_x - 80, center_y + 15)]
+        horizontal_right = [(center_x + 80, center_y - 15), (center_x + 80, center_y + 15)]
+        horizontal_top = [(center_x - 80, center_y - 15), (center_x + 80, center_y - 15)]
+        horizontal_bottom = [(center_x - 80, center_y + 15), (center_x + 80, center_y + 15)]
+
+        lines = [vertical_left_top, vertical_right_top, vertical_left_bot, vertical_right_bot, vertical_top,
+            vertical_bottom, horizontal_left, horizontal_right, horizontal_top, horizontal_bottom]
+
+        line_color = (0, 0, 255)
+        line_thickness = 2
+
+        for line in lines:
+            cv2.line(image, line[0], line[1], line_color, thickness=line_thickness)
+
+        res_frame = cv2.addWeighted(frame, 1, image, 0.5, 0)
+
+        cv2.imshow("cross", res_frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+task6()
