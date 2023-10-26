9920805778e50567f888a4d8f48c06a0ebb0ed0b
SammySatoro <ivzhenko2014@gmail.com>
2023-10-27 00:40:41 +0300

    task4

--- lab1/lab.py
+++ lab1/lab.py
@@ -28,6 +28,26 @@ def task3():
             exit()
 
 def task4():
+    vid_cap = cv2.VideoCapture("video/polish-cow.mp4", cv2.CAP_ANY)
+    width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter("task4_dir/polish-cow2.mp4", fourcc, 25, (width, height))
+
+    while True:
+        ret, vid = vid_cap.read()
+        if not ret:
+            break
+        cv2.imshow('polish cow', vid)
+        video_writer.write(vid)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    vid_cap.release()       # is optional here
+    video_writer.release()  # is optional here
+    cv2.destroyAllWindows()
+
+def task5():
     pass
 
-task3()
+task4()
